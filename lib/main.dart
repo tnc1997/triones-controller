02ea6a68374583ec6d7be698c4198d5a5bdeac12
Thomas Clark <tnc1997@outlook.com>
2024-08-03 19:45:06 +0100

    chore: update material app title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Controller for Triones',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFF00006E),
