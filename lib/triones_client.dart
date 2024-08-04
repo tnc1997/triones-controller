d23528187e1d6cb8e1cf1de81d9249b4ede7e584
Thomas Clark <tnc1997@outlook.com>
2024-08-04 16:36:11 +0100

    refactor: add triones modes

--- lib/triones_client.dart
+++ lib/triones_client.dart
@@ -18,6 +18,29 @@ class TrionesException implements Exception {
   const TrionesException();
 }
 
+class TrionesModes {
+  static const pulsatingRainbow = 0x25;
+  static const pulsatingRed = 0x26;
+  static const pulsatingGreen = 0x27;
+  static const pulsatingBlue = 0x28;
+  static const pulsatingYellow = 0x29;
+  static const pulsatingCyan = 0x2A;
+  static const pulsatingPurple = 0x2B;
+  static const pulsatingWhite = 0x2C;
+  static const pulsatingRedGreen = 0x2D;
+  static const pulsatingRedBlue = 0x2E;
+  static const pulsatingGreenBlue = 0x2F;
+  static const rainbowStrobe = 0x30;
+  static const redStrobe = 0x31;
+  static const greenStrobe = 0x32;
+  static const blueStrobe = 0x33;
+  static const yellowStrobe = 0x34;
+  static const cyanStrobe = 0x35;
+  static const purpleStrobe = 0x36;
+  static const whiteStrobe = 0x37;
+  static const rainbowJumpingChange = 0x38;
+}
+
 List<int>? _normalize(
   List<int>? uuid,
 ) {
