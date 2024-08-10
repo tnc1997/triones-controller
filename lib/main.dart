688a97ce792cac3d74ccd4bb4b9acdd1c78f499a
Thomas Clark <tnc1997@outlook.com>
2024-08-10 06:55:56 +0100

    refactor: replace characteristic with triones client

--- lib/main.dart
+++ lib/main.dart
@@ -59,15 +59,19 @@ class _ScanScreenState extends State<ScanScreen> {
                 return ListTile(
                   title: Text(results[index].device.platformName),
                   onTap: () async {
-                    await Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return DeviceScreen(
-                            device: results[index].device,
-                          );
-                        },
-                      ),
-                    );
+                    await results[index].device.connect();
+
+                    if (context.mounted) {
+                      await Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return DeviceScreen(
+                              device: results[index].device,
+                            );
+                          },
+                        ),
+                      );
+                    }
                   },
                 );
               },
@@ -143,7 +147,7 @@ class _DeviceScreenState extends State<DeviceScreen> {
   };
 
   double _brightness = 1;
-  late final Future<BluetoothCharacteristic?> _characteristic;
+  late final TrionesClient _client;
   Color _color = Colors.white;
   final _debouncer = Debouncer(duration: const Duration(milliseconds: 500));
   int? _mode;
@@ -155,160 +159,130 @@ class _DeviceScreenState extends State<DeviceScreen> {
       appBar: AppBar(
         title: Text(widget.device.platformName),
       ),
-      body: FutureBuilder(
-        future: _characteristic,
-        builder: (context, snapshot) {
-          if (snapshot.data case final characteristic?) {
-            return Padding(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+              child: ElevatedButton.icon(
+                onPressed: () async {
+                  if (_power) {
+                    await _client.turnOff();
+                  } else {
+                    await _client.turnOn();
+                  }
+
+                  setState(() {
+                    _power = !_power;
+                  });
+                },
+                icon: const Icon(Icons.power_settings_new),
+                label: const Text('Power'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton.icon(
+                onPressed: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (context) {
+                      return SimpleDialog(
+                        children: [
+                          ColorPicker(
+                            pickerColor: _color,
+                            onColorChanged: (color) {
+                              setState(() {
+                                _color = color;
+                              });
+
+                              _debouncer.run(() async {
+                                final color = _color.withBrightness(
+                                  _brightness,
+                                );
+
+                                await _client.setColor(
+                                  color.red,
+                                  color.green,
+                                  color.blue,
+                                );
+                              });
+                            },
+                            enableAlpha: false,
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                icon: const Icon(Icons.color_lens),
+                label: const Text('Color'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton.icon(
-                      onPressed: () async {
-                        await characteristic.write([
-                          0xCC,
-                          _power ? 0x24 : 0x23,
-                          0x33,
-                        ]);
+                  const Icon(Icons.brightness_low),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                      ),
+                      child: Slider(
+                        value: _brightness,
+                        onChanged: (brightness) {
+                          setState(() {
+                            _brightness = brightness;
+                          });
 
-                        setState(() {
-                          _power = !_power;
-                        });
-                      },
-                      icon: const Icon(Icons.power_settings_new),
-                      label: const Text('Power'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton.icon(
-                      onPressed: () async {
-                        await showDialog(
-                          context: context,
-                          builder: (context) {
-                            return SimpleDialog(
-                              children: [
-                                ColorPicker(
-                                  pickerColor: _color,
-                                  onColorChanged: (color) {
-                                    setState(() {
-                                      _color = color;
-                                    });
-
-                                    _debouncer.run(() async {
-                                      final color = _color.withBrightness(
-                                        _brightness,
-                                      );
-
-                                      await characteristic.write([
-                                        0x56,
-                                        color.red,
-                                        color.green,
-                                        color.blue,
-                                        0x00,
-                                        0xF0,
-                                        0xAA,
-                                      ]);
-                                    });
-                                  },
-                                  enableAlpha: false,
-                                ),
-                              ],
+                          _debouncer.run(() async {
+                            final color = _color.withBrightness(
+                              _brightness,
                             );
-                          },
-                        );
-                      },
-                      icon: const Icon(Icons.color_lens),
-                      label: const Text('Color'),
+
+                            await _client.setColor(
+                              color.red,
+                              color.green,
+                              color.blue,
+                            );
+                          });
+                        },
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        const Icon(Icons.brightness_low),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0,
-                            ),
-                            child: Slider(
-                              value: _brightness,
-                              onChanged: (brightness) {
-                                setState(() {
-                                  _brightness = brightness;
-                                });
-
-                                _debouncer.run(() async {
-                                  final color = _color.withBrightness(
-                                    _brightness,
-                                  );
-
-                                  await characteristic.write([
-                                    0x56,
-                                    color.red,
-                                    color.green,
-                                    color.blue,
-                                    0x00,
-                                    0xF0,
-                                    0xAA,
-                                  ]);
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                        const Icon(Icons.brightness_high),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: DropdownMenu(
-                      label: const Text('Mode'),
-                      onSelected: (mode) async {
-                        if (mode != null) {
-                          await characteristic.write([
-                            0xBB,
-                            mode,
-                            0x00,
-                            0x44,
-                          ]);
-                        }
-
-                        setState(() {
-                          _mode = mode;
-                        });
-                      },
-                      expandedInsets: EdgeInsets.zero,
-                      dropdownMenuEntries: [
-                        for (final entry in _modes.entries)
-                          DropdownMenuEntry(
-                            value: entry.key,
-                            label: entry.value,
-                          ),
-                      ],
-                    ),
-                  ),
+                  const Icon(Icons.brightness_high),
                 ],
               ),
-            );
-          }
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownMenu(
+                label: const Text('Mode'),
+                onSelected: (mode) async {
+                  if (mode != null) {
+                    await _client.setMode(mode);
+                  }
 
-          if (snapshot.error case final error?) {
-            return Center(
-              child: Text('$error'),
-            );
-          }
-
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+                  setState(() {
+                    _mode = mode;
+                  });
+                },
+                expandedInsets: EdgeInsets.zero,
+                dropdownMenuEntries: [
+                  for (final entry in _modes.entries)
+                    DropdownMenuEntry(
+                      value: entry.key,
+                      label: entry.value,
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -326,19 +300,11 @@ class _DeviceScreenState extends State<DeviceScreen> {
   void initState() {
     super.initState();
 
-    _characteristic = widget.device.connect().then((_) async {
-      for (final service in await widget.device.discoverServices()) {
-        if (service.uuid == Guid('FFD5')) {
-          for (final characteristic in service.characteristics) {
-            if (characteristic.uuid == Guid('FFD9')) {
-              return characteristic;
-            }
-          }
-        }
-      }
-
-      return null;
-    });
+    _client = TrionesClient(
+      device: FlutterBluePlusBluetoothDevice(
+        device: widget.device,
+      ),
+    );
   }
 }
 
